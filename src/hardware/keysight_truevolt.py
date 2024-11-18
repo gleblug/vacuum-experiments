6a30f1f43130a4df88a415c00bb86e0ff96bc2ad
unknown <galera951@gmail.com>
2024-11-18 15:22:48 +0300

    Small fixes

--- src/hardware/keysight_truevolt.py
+++ src/hardware/keysight_truevolt.py
@@ -7,6 +7,7 @@ from hardware.interfaces import MultiStatus
 class KeysightTruevolt:
 
 	instr: scpi.Instrument
+	port: str
 	status: MultiStatus
 	min_range: float
 	max_range: float
@@ -15,11 +16,14 @@ class KeysightTruevolt:
 	def __init__(self, port: str):
 		self.measure_time = .41 # seconds to one measurement
 		self.status = MultiStatus.unknown
+		self.port = port
+		self.connect()
 
-		self.instr = scpi.Instrument(port)
+	def connect(self):
+		self.instr = scpi.Instrument(self.port)
 		self.instr.connect()
 		self.setMsg("MEASURING")
-		
+
 	def __del__(self):
 		self.instr.write('SYST:LAB ""')
 		self.instr.disconnect()
@@ -28,22 +32,23 @@ class KeysightTruevolt:
 		self.instr.write(f'SYST:LAB "{msg}"')
 
 	def current(self) -> float:
-		self._confCurrentDC()
+		query = 'CURR:DC'
+		if self.status != MultiStatus.currentDC:
+			self._configure(query)
+		return self._getValueUpdRanges(query)
 
-		value = float(self.instr.query(f'MEAS:CURR:DC? {self.cur_range:E}'))
-		if ((value > .8 * self.cur_range) and (not math.isclose(self.cur_range, self.max_range))) \
-			or ((value < .1 * self.cur_range) and (not math.isclose(self.cur_range, self.min_range))):
-			value = float(self.instr.query(f'MEAS:CURR:DC? AUTO'))
-			self.cur_range = float(self.instr.query('CURR:DC:RANGE?'))
-		return value
-
-	def _confCurrentDC(self) -> None:
-		if self.status == MultiStatus.currentDC:
-			return
-		self.instr.write('CONF:CURR:DC')
+	def _configure(self, query: str) -> None:
+		self.instr.write(f'CONF:{query}')
 		self.status = MultiStatus.currentDC
 
-		self.min_range = float(self.instr.query('CURR:DC:RANGE? MIN'))
-		self.max_range = float(self.instr.query('CURR:DC:RANGE? MAX'))
+		self.min_range = float(self.instr.query(f'{query}:RANGE? MIN'))
+		self.max_range = float(self.instr.query(f'{query}:RANGE? MAX'))
 		self.cur_range = self.max_range
 
+	def _getValueUpdRanges(self, query: str) -> float:
+		value = float(self.instr.query(f'MEAS:{query}? {self.cur_range:E}'))
+		if ((value > .8 * self.cur_range) and (not math.isclose(self.cur_range, self.max_range))) \
+			or ((value < .1 * self.cur_range) and (not math.isclose(self.cur_range, self.min_range))):
+			value = float(self.instr.query(f'MEAS:{query}? AUTO'))
+			self.cur_range = float(self.instr.query(f'SENS:{query}:RANGE?'))
+		return value
