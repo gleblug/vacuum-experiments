4f758ecbc6f8a5d4ae36ff540c098131c801bc29
unknown <galera951@gmail.com>
2024-11-18 15:22:48 +0300

    Small fixes

--- src/measurement.py
+++ src/measurement.py
@@ -3,26 +3,42 @@ import math
 import time
 from hardware.interfaces import Ammeter
 from hardware.keysight_truevolt import KeysightTruevolt
+import logging
+import sys
 
-def measureCurrent(amp: Ammeter, filename: str):
+
+def writeCurrent(amp: Ammeter, filename: str):
 	value = amp.current()
-	now_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+	now_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
 	with open(filename, 'a') as f:
 		f.write(f'{now_time}\t{value}\n')
 		
-def main():
-	filename = f"keysight_data_{datetime.now().strftime("%Y-%m-%d_%H-%M-%s")}.csv"
+def measure():
+	filename = f"../data/keysight_data_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.csv"
 	keysight = KeysightTruevolt("USB0")
+	with open(filename, "w") as f:
+		f.write('time\tcurrent[A]\n')
 	while True:
 		try:
-			measureCurrent(keysight, filename)
+			writeCurrent(keysight, filename)
 
 			time_until_next = (1e6 - datetime.now().microsecond) / 1e6 - keysight.measure_time
 			if time_until_next < 0:
 				time_until_next -= math.floor(time_until_next)
 			time.sleep(time_until_next)
-		except ValueError:
+		except KeyboardInterrupt:
+			logging.info("STOP")
+			return
+		except ValueError as e:
+			logging.warning("Error while parsing value", e)
 			continue
+		except Exception as e:
+			logging.critical("STOP", e)
+			return
+
+def main():
+	logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(asctime)s %(levelname)s %(message)s")
+	measure()
 
 if __name__ == '__main__':
 	main()
