9224fb6e0f89d44224ec2360ceb576a9e27df325
unknown <galera951@gmail.com>
2024-11-27 17:29:29 +0300

    Argparse config.ini file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 import logging
 import sys
 import configparser
+import argparse
 
 from hardware.interfaces import Status
 from hardware.multimeter_scpi import MultimeterSCPI
 from measurer import Measurer
-from hardware.keysight_proxy import KeysightProxy
-from hardware.rigol_proxy import RigolProxy
 
 def main():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(asctime)s %(levelname)s %(message)s")
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_path', help='Path to config.ini file.')
+    args = parser.parse_args()
+
     logging.info("Load config...")
     config = configparser.ConfigParser()
-    config.read('config.ini')
-    t = config.getfloat('Measurer', 'timeout')
+    config.read(args.config_path)
 
     logging.info("Initialize hardware...")
     hardware_sections = [(config[s], s.split('.')[1]) for s in config.sections() if s.split('.')[0] == 'Hardware']
@@ -28,7 +30,9 @@ def main():
         meters.append(meter)
         logging.info(f'{name} connected.')
 
-    measurer = Measurer(meters=meters, timeout=t)
+    t = config.getfloat('Measurer', 'Timeout')
+    d = config['Measurer']['Directory']
+    measurer = Measurer(directory=d, meters=meters, timeout=t)
     measurer.measure()
 
 if __name__ == '__main__':
